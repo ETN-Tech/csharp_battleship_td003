0aee20958c99063ee85444e80873ac915aa819c6
Hono <honorine.f@hotmail.com>
2021-02-15 15:13:41 +0100

    Gamer up

--- BatNav/Gamer.cs
+++ BatNav/Gamer.cs
@@ -1,14 +1,40 @@
 ﻿// importation des espaces de nommage utiles
 using System;
 
+
 // Ajout de classes dans l'espace de nommage BatNav de l'application
 namespace BatNav
 {
+    public class Guid
+    {
+        public void NewGuid()
+        {
+            Random aleatoire = new Random();
+            int id = aleatoire.next(0000, 9999);
+        }
+    }
+
+    public class DateTime
+    {
+
+    }
+
     public class Gamer
     {
         // @TODO010 CLASSE
 
+        private Guid id { get; set; }
+        public string pseudo { get; set; }
+        public DateTime _creationDate;
 
+        public ID(Guid id)
+        {
+
+        }
+
+        public unknown(string pseudo, Guid id)
+        {
+
+        }
     }
-	
 }
